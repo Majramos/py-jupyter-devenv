9a796814e06fb3ab40c1612b7e496a191d2f73a6
Majramos <majramos@gmail.com>
2022-04-19 12:32:03 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,11 @@
 #!/usr/bin/env bash
 
-rm -rf ./git
+SCRIPT_PATH="$(dirname "${BASH_SOURCE[0]}")"
+
+# remove unwanted folders from git
+rm -rf $SCRIPT_PATH/.git
+rm -rf $SCRIPT_PATH/.gitignore
+rm -rf $SCRIPT_PATH/CHANGELOG.md
+rm -rf $SCRIPT_PATH/README.md
+rm -rf $SCRIPT_PATH/LICENSE
+rm -rf $SCRIPT_PATH/install.sh
