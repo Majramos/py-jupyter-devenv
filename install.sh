b226ecc40ba3b6243235f9d4d9d7b453c7e4beda
Marco Ramos <majramos@gmail.com>
2022-06-09 10:43:45 +0100

    add .gitlab-ci.yml to install file

--- install.sh
+++ install.sh
@@ -5,6 +5,7 @@ SCRIPT_PATH="$(dirname "${BASH_SOURCE[0]}")"
 # remove unwanted folders from git
 rm -rf $SCRIPT_PATH/.git
 rm -rf $SCRIPT_PATH/.gitignore
+rm -rf $SCRIPT_PATH/.gitlab-ci.yml
 rm -rf $SCRIPT_PATH/CHANGELOG.md
 rm -rf $SCRIPT_PATH/README.md
 rm -rf $SCRIPT_PATH/LICENSE
